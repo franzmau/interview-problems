bebc135549e45b40d08abb3c38ffc9851a0f1861
Armando Ferreira <armando.ferreira@oracle.com>
2017-10-17 09:59:44 -0500

    Easy: Palidrome Problem
    Added the classic palindrome problem to the project.

--- src/com/interview/problems/easy/Palindrome.java
+++ src/com/interview/problems/easy/Palindrome.java
@@ -2,7 +2,25 @@ package com.interview.problems.easy;
 
 public class Palindrome {
 
+    /**
+     * Given a string S return True if its a "Palindrome", False if its not.
+     *
+     * @param word String to check.
+     * @return True if its a palindrome, False if its not.
+     */
+    private static boolean isPalindrome(String word) {
+        int length = word.length();
+        for (int i = 0; i < length/2; i++) {
+            if (word.charAt(i) != word.charAt(length-(i+1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(isPalindrome("kayak"));
+        System.out.println(isPalindrome("armando"));
+        System.out.println(isPalindrome("tattarrattat"));
     }
 }
